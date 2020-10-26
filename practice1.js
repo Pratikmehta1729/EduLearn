8d1b946c5c7a88053a51be15e60708fc0a0ee66e
Pratikmehta1729 <pratikjain17291729@gmail.com>
2020-10-26 11:26:13 +0530

    Update

--- practice1.js
+++ practice1.js
@@ -1,1 +1,17 @@
 // How to make object in js
+let obj1 = {
+    // fields
+      name:"value",
+      num: 123,
+    //methods
+      foo: function(){}
+      
+    }
+
+// iterate Object keys
+Object.keys(obj1).forEach((key) => { 
+    console.log(key ," : ",obj1[key])
+})
+
+
+
