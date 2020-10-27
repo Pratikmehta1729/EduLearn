6ab4e42fc7bfec84a6da0ebce89188c3a21d95a4
Pratikmehta1729 <pratikjain17291729@gmail.com>
2020-10-27 17:14:14 +0530

    Updates (Commiting and add both with single command)

--- practice1.js
+++ practice1.js
@@ -14,5 +14,11 @@ Object.keys(obj1).forEach((key) => {
     console.log(key ," : ",obj1[key])
 })
 
+//Array in js
+var arr1 = [1,2,3,4,"hello"];
+for(var i=0; i<arr1.length; i++)
+{
+  console.log(arr1[i]);
+}
 
 
